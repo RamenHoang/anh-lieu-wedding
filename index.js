9aa00cb749bb2fcc9fd541f2e12f025e629322f1
Nguyen Hoang Anh <anhnguyenhoang321@gmai.com>
2022-12-21 15:33:12 +0000

    Create entry file and add layouts

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname + '/rosy.day'))
+
+app.get('/', (req, res) => {
+	res.send('Hello, world!!!')
+})
+
+app.listen(80, () => {
+	console.log(`Example app listening on port 80`)
+})
