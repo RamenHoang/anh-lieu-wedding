60ac8ca979175f04dc4c1e7fe2ee018e9672c707
RamenHoang <anhnguyenhoang321@gmail.com>
2023-01-07 21:03:08 +0700

    Add validation for send wish

--- public/static/wishes.js
+++ public/static/wishes.js
@@ -1,3 +1,46 @@
+function removeInputErrorStyle(e) {
+  if (e.target != this) {
+    return;
+  }
+
+  if (this.classList.contains("input-error")) {
+    this.classList.remove("input-error");
+  }
+}
+
+function sendWish(senderName, senderContent, senderEmail, wishes) {
+  return function (e) {
+    e.preventDefault();
+
+    if (!senderName.value) {
+      senderName.classList.add("input-error");
+      senderName.focus();
+      return;
+    } else if (!senderContent.value) {
+      senderContent.classList.add("input-error");
+      senderContent.focus();
+      return;
+    } else {
+      axios
+        .post("/wishes", {
+          name: senderName.value,
+          email: senderEmail.value,
+          content: senderContent.value,
+        })
+        .then(function (response) {
+          senderName.value = null;
+          senderEmail.value = null;
+          senderContent.value = null;
+
+          wishes.innerHTML = response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  };
+}
+
 function init() {
   var sendWishBtn = document.getElementById("send-wish-btn-idx");
   var senderName = document.getElementById("username-wish-idx");
@@ -5,24 +48,16 @@ function init() {
   var senderContent = document.getElementById("content-wish-idx");
   var wishes = document.getElementById("view-wish-idx");
 
-  sendWishBtn.addEventListener("click", function () {
-    axios
-      .post("/wishes", {
-        name: senderName.value,
-        email: senderEmail.value,
-        content: senderContent.value,
-      })
-      .then(function (response) {
-        senderName.value = null;
-        senderEmail.value = null;
-        senderContent.value = null;
+  senderName.addEventListener("keyup", removeInputErrorStyle.bind(senderName));
+  senderContent.addEventListener(
+    "keyup",
+    removeInputErrorStyle.bind(senderContent)
+  );
 
-        wishes.innerHTML = response.data;
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  });
+  sendWishBtn.addEventListener(
+    "click",
+    sendWish(senderName, senderContent, senderEmail, wishes)
+  );
 }
 
 init();
