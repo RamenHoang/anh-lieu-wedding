9365364130d7abc6d8856f1f2ebc9362de8366da
RamenHoang <anhnguyenhoang321@gmail.com>
2024-03-22 14:35:46 +0700

    Remove slash character for github page can load resource

---
+++ public/static/js/wishes_html.js
@@ -0,0 +1,65 @@
+function removeInputErrorStyle(e) {
+  if (e.target != this) {
+    return;
+  }
+
+  if (this.classList.contains("input-error")) {
+    this.classList.remove("input-error");
+  }
+}
+
+function sendWish(senderName, senderContent, wishes) {
+  return function (e) {
+    e.preventDefault();
+
+    if (!senderName.value) {
+      senderName.classList.add("input-error");
+      senderName.focus();
+      return;
+    } else if (!senderContent.value) {
+      senderContent.classList.add("input-error");
+      senderContent.focus();
+      return;
+    } else {
+      axios
+        .post("/wishes", {
+          name: senderName.value,
+          content: senderContent.value,
+        })
+        .then(function (response) {
+          senderName.value = null;
+          senderContent.value = null;
+
+          wishes.innerHTML = response.data;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  };
+}
+
+function init() {
+  var sendWishBtn = document.getElementById("send-wish-btn-idx");
+  var senderName = document.getElementById("username-wish-idx");
+  var senderContent = document.getElementById("content-wish-idx");
+  var wishes = document.getElementById("view-wish-idx");
+
+  if (senderName) {
+    senderName.addEventListener("keyup", removeInputErrorStyle.bind(senderName));
+  }
+  if (senderContent) {
+    senderContent.addEventListener(
+      "keyup",
+      removeInputErrorStyle.bind(senderContent)
+    );
+  }
+  if (sendWishBtn) {
+    sendWishBtn.addEventListener(
+      "click",
+      sendWish(senderName, senderContent, wishes)
+    );
+  }
+}
+
+init();
